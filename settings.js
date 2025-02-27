98eaaf0d6c7363b7d714c3aecb51cb8c76ddfa5d
Zezeto01 <90875788+Zezeto01@users.noreply.github.com>
2025-02-27 16:44:34 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Kingsilver225, // Your name
+  ownerNumber: '2250586851734', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
